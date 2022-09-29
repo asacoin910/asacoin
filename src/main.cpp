942c253b220a87cfb68f33a3f19f05f1e93fbe0b
asacoin910 <113351452+asacoin910@users.noreply.github.com>
2022-09-29 21:56:17 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3139,7 +3139,7 @@ bool CheckWork(const CBlock block, CBlockIndex* const pindexPrev)
 
         unsigned int nBitsRequired = GetNextWorkRequired(pindexPrev, &block);
 
-    if (block.nBits != nBitsRequired) {
+    if (block.nBits < nBitsRequired) { // we can accept blocks with *more* work bits
         return error("%s : incorrect proof of work at %d", __func__, pindexPrev->nHeight + 1);
     }
 
